2ec0221f66e488362d6a25b9c014188bcf90677f
FcRig <69701326+FcRig@users.noreply.github.com>
2025-06-17 15:02:48 -0300

    Enhance logout mutation

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,14 +2,25 @@ import { createStore } from 'vuex';
 
 export default createStore({
   state: {
-    user: null
+    user: null,
+    token: localStorage.getItem('token')
   },
   mutations: {
     setUser(state, user) {
       state.user = user;
     },
+    setToken(state, token) {
+      state.token = token;
+      if (token) {
+        localStorage.setItem('token', token);
+      } else {
+        localStorage.removeItem('token');
+      }
+    },
     logout(state) {
       state.user = null;
+      state.token = null;
+      localStorage.removeItem('token');
     }
   },
   actions: {},
