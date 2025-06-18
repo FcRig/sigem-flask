2ed265dcecfc36f9b71383b532fba87988f84994
FcRig <69701326+FcRig@users.noreply.github.com>
2025-06-18 08:33:12 -0300

    Comentários em config

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,8 @@
 import os
+from datetime import timedelta
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "jdeiie760zxzxfd$$#2*kkkdytsa")
     SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False  # Alterar em produção!!!
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "kdjkjfuirahfahuiiah&$hfhffh054545#")
+    JWT_ACCESS_TOKEN_EXPIRES = False  # Alterar em produção!!!
