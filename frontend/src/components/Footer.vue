c280e8aa6baae9efbb7da0131d6ed83f13105891
FcRig <69701326+FcRig@users.noreply.github.com>
2025-06-18 18:29:34 -0300

    alterando footer

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
-  <v-footer app color="#f3d712" class="footer">
-    <span class="mx-auto">&copy; 2025 - SIGEM</span>
+  <v-footer app color="#f3d712" class="d-flex justify-start">
+    <span class="mx-auto">&copy; 2025 - Superintendência da PRF no Rio Grande do Sul</span>
   </v-footer>
 </template>
 
