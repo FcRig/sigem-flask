cce02628cc1efa8aeca0c3940217e6237d402c85
LeoAndrighetto <leoandrighetto02@gmail.com>
2025-12-05 10:03:51 -0300

    @feat Arquivos backend para teste de cadastro de usuário em código

---
+++ backend/create_adm.py
@@ -0,0 +1,17 @@
+from app import create_app
+from app.extensions import db
+from app.models import User
+from werkzeug.security import generate_password_hash
+
+app = create_app()
+
+with app.app_context():
+    user = User(
+        username="leoadm",
+        email="leoadm@gmail.com",
+        password_hash=generate_password_hash("Leo-2537?"),
+        administrador=True
+    )
+    db.session.add(user)
+    db.session.commit()
+    print("Usuário admin criado com sucesso!")
